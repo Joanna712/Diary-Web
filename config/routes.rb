5fa46f5b246b47a0f8439e25396b600a33fc059e
Blues <qingfengorlangyue@gmail.com>
2013-10-20 23:28:44 +0800

    modify routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 DiaryWeb::Application.routes.draw do
-  get "welcome/index"
-  get "welcome/about"
-  get "welcome/help"
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   root 'welcome#index'
+  match '/about', to: 'welcome#about', via: get
+  match '/help', to: 'welcome#help', via: get
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
